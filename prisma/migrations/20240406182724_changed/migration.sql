af707a123f5571bfc70bbe1ac702cbda01d09385
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2024-04-16 20:19:50 +0300

    changed the directory location

--- prisma/migrations/20240406182724_changed/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `registration_timestamp` on the `users` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "registration_timestamp",
-ADD COLUMN     "registration_timestamp" INTEGER NOT NULL;
